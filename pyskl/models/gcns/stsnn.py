a0fbbe84d53edb849a09dbb16891723b37cdad0c
GiovanniAdelfio <147213043+GiovanniAdelfio@users.noreply.github.com>
2026-05-21 12:08:09 +0200

    Add files via upload

---
+++ pyskl/models/gcns/stsnn.py
@@ -0,0 +1,153 @@
+import copy as cp
+import torch
+import torch.nn as nn
+from mmcv.runner import load_checkpoint
+from ...utils import Graph, cache_checkpoint
+from ..builder import BACKBONES
+from .utils import mstcn, unit_tcn
+
+
+from  .utils import SheafNN 
+EPS = 1e-4
+
+
+class STSNNBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 A,
+                 stride=1,
+                 residual=True,
+                 device = 'cuda',
+                 **kwargs):
+        super().__init__()
+
+        # Estrazione edge_index dalla matrice A di STGCN
+        # A di solito è (K, 25, 25). Facciamo la somma per collassare K e troviamo gli indici non nulli.
+        if A.dim() == 3:
+            A_base = A.sum(dim=0)
+        else:
+            A_base = A
+            
+        edge_index = A_base.nonzero(as_tuple=False).t().contiguous()
+        num_nodes = A_base.size(0)
+
+        gcn_kwargs = {k[4:]: v for k, v in kwargs.items() if k[:4] == 'gcn_'}
+        tcn_kwargs = {k[4:]: v for k, v in kwargs.items() if k[:4] == 'tcn_'}
+        tcn_type = tcn_kwargs.pop('type', 'unit_tcn')
+        
+        # INNESTO DELLA SHEAF NN
+        # Inserisci i parametri della tua Sheaf (stalk, n_layers) a piacimento
+        self.gcn = SheafNN(
+            in_channels=in_channels, 
+            out_channels=out_channels, 
+            edge_index=edge_index, 
+            num_nodes=num_nodes,
+            stalk=2, 
+            n_layers=2,
+            device = device,
+            res = gcn_kwargs.get("with_res", False),
+            ort = gcn_kwargs.get("orthogonal", True)
+        )
+
+        if tcn_type == 'unit_tcn':
+            self.tcn = unit_tcn(out_channels, out_channels, 9, stride=stride, **tcn_kwargs)
+        elif tcn_type == 'mstcn':
+            self.tcn = mstcn(out_channels, out_channels, stride=stride, **tcn_kwargs)
+            
+        self.relu = nn.ReLU()
+
+        if not residual:
+            self.residual = lambda x: 0
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+        else:
+            self.residual = unit_tcn(in_channels, out_channels, kernel_size=1, stride=stride)
+
+    def forward(self, x, A=None):
+        res = self.residual(x)
+        x = self.tcn(self.gcn(x)) + res
+        return self.relu(x)
+
+@BACKBONES.register_module()
+class STSNN(nn.Module):
+
+    def __init__(self,
+                 graph_cfg,
+                 in_channels=3,
+                 base_channels=64,
+                 data_bn_type='VC',
+                 ch_ratio=2,
+                 num_person=2,  # * Only used when data_bn_type == 'MVC'
+                 num_stages=10,
+                 inflate_stages=[5, 8],
+                 down_stages=[5, 8],
+                 pretrained=None,
+                 **kwargs):
+        super().__init__()
+
+        self.graph = Graph(**graph_cfg)
+        A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
+        self.data_bn_type = data_bn_type
+        self.kwargs = kwargs
+
+        if data_bn_type == 'MVC':
+            self.data_bn = nn.BatchNorm1d(num_person * in_channels * A.size(1))
+        elif data_bn_type == 'VC':
+            self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
+        else:
+            self.data_bn = nn.Identity()
+
+        lw_kwargs = [cp.deepcopy(kwargs) for i in range(num_stages)]
+        for k, v in kwargs.items():
+            if isinstance(v, tuple) and len(v) == num_stages:
+                for i in range(num_stages):
+                    lw_kwargs[i][k] = v[i]
+        lw_kwargs[0].pop('tcn_dropout', None)
+
+        self.in_channels = in_channels
+        self.base_channels = base_channels
+        self.ch_ratio = ch_ratio
+        self.inflate_stages = inflate_stages
+        self.down_stages = down_stages
+
+        modules = []
+        if self.in_channels != self.base_channels:
+            modules = [STSNNBlock(in_channels, base_channels, A.clone(), 1, residual=False, **lw_kwargs[0])]
+
+        inflate_times = 0
+        for i in range(2, num_stages + 1):
+            stride = 1 + (i in down_stages)
+            in_channels = base_channels
+            if i in inflate_stages:
+                inflate_times += 1
+            out_channels = int(self.base_channels * self.ch_ratio ** inflate_times + EPS)
+            base_channels = out_channels
+            modules.append(STSNNBlock(in_channels, out_channels, A.clone(), stride, **lw_kwargs[i - 1]))
+
+        if self.in_channels == self.base_channels:
+            num_stages -= 1
+
+        self.num_stages = num_stages
+        self.gcn = nn.ModuleList(modules)
+        self.pretrained = pretrained
+
+    def init_weights(self):
+        if isinstance(self.pretrained, str):
+            self.pretrained = cache_checkpoint(self.pretrained)
+            load_checkpoint(self, self.pretrained, strict=False)
+
+    def forward(self, x):
+        N, M, T, V, C = x.size()
+        x = x.permute(0, 1, 3, 4, 2).contiguous()
+        if self.data_bn_type == 'MVC':
+            x = self.data_bn(x.view(N, M * V * C, T))
+        else:
+            x = self.data_bn(x.view(N * M, V * C, T))
+        x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
+
+        for i in range(self.num_stages):
+            x = self.gcn[i](x)
+
+        x = x.reshape((N, M) + x.shape[1:])
+        return x
